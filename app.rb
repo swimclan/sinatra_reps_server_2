869d8d01fd149e571b2be5463a57e1ff8802e4ae
Matt Herron <matthew.herron77@gmail.com>
2015-07-24 15:51:05 -0500

    Commited all the apis for fake songs

--- app.rb
+++ app.rb
@@ -1,18 +1,22 @@
 require 'bundler'
 Bundler.require()
 
-get '/' do
-  
+def fake_song(title, release_year, artist)
+  {:title => title, :release_year => release_year, :artist => artist}
 end
 
 get '/' do
-
+  '<h1>This is not a page that you should be at</h1><p>Please <a href="/">RTFM</a>!!!</p>'
 end
 
-get '/' do
-
+get '/api/heat' do
+  fake_song('Feel The Heat', 1986, 'Miranda Gardens').to_json
 end
 
-get '/' do
+get '/api/garden' do
+  fake_song('Play In My Garden ', 1990, 'Felicia Waxler').to_json
+end
 
+get '/api/hands' do
+  fake_song('Feel My Hands', 2001, 'Tasha Underman').to_json
 end
