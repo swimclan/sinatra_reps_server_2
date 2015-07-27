45943af1126df240f53dfcf793fda00780ed7b9c
Matt Herron <matthew.herron77@gmail.com>
2015-07-26 23:52:13 -0500

    Needed to support cross origin requests

--- app.rb
+++ app.rb
@@ -10,13 +10,16 @@ get '/' do
 end
 
 get '/api/heat' do
+  cross_origin
   fake_song('Feel The Heat', 1986, 'Miranda Gardens').to_json
 end
 
 get '/api/garden' do
+  cross_origin
   fake_song('Play In My Garden ', 1990, 'Felicia Waxler').to_json
 end
 
 get '/api/hands' do
+  cross_origin
   fake_song('Feel My Hands', 2001, 'Tasha Underman').to_json
 end
